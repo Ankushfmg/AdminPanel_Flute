2ca43182100b2f6710188160671c4bf689763614
Ankushfmg <qalead@funmaxgames.com>
2024-06-03 16:28:53 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Testing!");
-
+        System.out.println("Pull Successfully!");
         for (int i = 1; i <= 5; i++){
             System.out.println("i = " + i);
         }
